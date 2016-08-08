53289da7941b70236ec028da19aa54fd3ae0c49d
Zonas Sun <zbsun@zonasoffice.ccoffice.avepoint.com>
2016-08-08 14:01:51 +0800

    auto commit

--- nodejs-test/test.js
+++ nodejs-test/test.js
@@ -1,5 +1,18 @@
 var jira = require("./node-jira");
-jira.jira_get("/rest/api/2/project")
+var p = {};
+jira.jira_get("/rest/api/2/myself")
     .then((d)=>{
-            console.log(d);
+            console.log(d.name);
+            return d.name;
+        })
+    .then((name)=>{
+                return Promise.all([jira.jira_get("/rest/api/2/search?jql="+encodeURIComponent("assignee="+name+" and resolution=Unresolved")+"&maxResults=0"),
+                                  jira.jira_get("/rest/api/2/search?jql="+encodeURIComponent("assignee="+name)+"&maxResults=0")]);
+        })
+    .then((result)=>{
+                console.log(result[0].total*100/result[1].total);
+                
+        })
+    .catch((e)=>{
+                console.log(e);
         });
