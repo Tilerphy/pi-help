ad77a70e3c913952719f5e4ec86c89b67c46af86
Zonas Sun <zbsun@zonasoffice.ccoffice.avepoint.com>
2016-08-15 14:19:14 +0800

    auto commit

--- c-test/base/sizeof.c
+++ c-test/base/sizeof.c
@@ -10,11 +10,19 @@ int main(){
     char* _char_p = "12345";
     char _char_a[] = "123456";
     //overflow
-    char _char_a_b[6] = "123456"; // 7
+    char _char_a_b[7] = "123456"; // 7
     int _int_p[10] = {1,2,3,4,5,6,7,8,9,0};
     unsigned long _ul = 100;
     unsigned int _ui = 100;
     int* malloc_int_p = (int*) malloc(100);
+    //auto alignment 内存对齐
+    struct pet_define {
+            int name;
+            char type[5];
+            int age;
+        } Pet;
+    struct pet_define cat;
+    
     printf("int %d\n" , sizeof(10));
     printf("int %d\n" , sizeof(_int));
     printf("short %d\n" , sizeof(_short));
@@ -32,5 +40,8 @@ int main(){
     printf("unsigned long %d\n" , sizeof(_ul));
     printf("unsigned int %d\n" , sizeof(_ui));
     printf("malloc() for int pointers %d\n", sizeof(malloc_int_p));
+    printf("struct: %d\n", sizeof(Pet));
+    printf("struct: %d. name is start at %p, age is start at %p", sizeof(cat), &cat.name, &cat.age);
+    
     return 0;
 }
